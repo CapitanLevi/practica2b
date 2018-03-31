20a582e34976fc1b008b4e2e36d4543735b9ebd4
CapitanLevi <36543483+CapitanLevi@users.noreply.github.com>
2018-03-31 12:42:22 -0500

    Listo

--- Problema15/main.cpp
+++ Problema15/main.cpp
@@ -14,15 +14,39 @@ Si se ingresan los rectángulos A y B representados por los arreglos
 using namespace std;
 void pedirDatos(double&,double&,double&,double&,double&,double&,double&,double&);
 bool validarInterseccion(double *,double *);
+void hallarInterseccion(double *, double *,double&,double&,double&,double&);
 
 int main(){
     double X1=0,X2=0,X3=0,Y1=0,Y2=0,Y3=0,W1=0,W2=0,W3=0,H1=0,H2=0,H3=0;
     pedirDatos(X1,Y1,W1,H1,X2,Y2,W2,H2);
-    double A[4]={X1,Y1,W1,H1},B[4]={X2,Y2,W2,H2}, C[4]={X3,Y3,W3,H3};
+    double A[4]={X1,Y1,W1,H1},B[4]={X2,Y2,W2,H2};
     if(!validarInterseccion(A,B)){
         cout<<"No hay intersección."<<endl;
         return 0;}
+    hallarInterseccion(A,B,X3,Y3,W3,H3);
+    double C[4]={X3,Y3,W3,H3};
+    cout<<"El rectángulo intersección C es arreglo: ";
+    cout<<'{'<<*C<<','<<*(C+1)<<','<<*(C+2)<<','<<*(C+3)<<'}'<<endl;
+    return 0;
+}
 
+void hallarInterseccion(double *a, double *b,
+                        double& x3,double& y3,double& w3,double& h3){
+    //Las coordenadas de C son las coordenadas máximas entre A y B.
+    //C[0]=max(A[0],B[0]);
+    x3=max(*a,*b);
+    //C[1]=max(A[1],B[1]);
+    y3=max(*(a+1),*(b+1));
+
+    /*El alto y el ancho de van hasta las esquínas mínimas (superior derecha
+        e inferior izquierda) entre A y B.*/
+    //Coordenadas X e Y de las esquinas de A y B.
+    //X2A=X1+W1  ; Y2A=Y2+H2
+    double X2A=*a+*(a+2); double Y2A=*(a+1)+*(a+3);
+    double X2B=*b+*(b+2); double Y2B=*(b+1)+*(b+3);
+    double X2C=min(X2A,X2B); double Y2C=min(Y2A,Y2B);
+    //Ancho y altura de C
+    w3=X2C-x3; h3=Y2C-y3;
 }
 
 bool validarInterseccion(double *a, double *b){
